b884abcb3130edcb01ad047165d452410d54dc92
Sneakls <sneakls@sneakls.st>
2025-10-13 09:57:17 +0200

    Create oobeacc.cmd

---
+++ oobeacc.cmd
@@ -0,0 +1,22 @@
+@echo off
+
+goto GETOPTS
+
+:GETOPTS
+IF "%~1"=="" goto NoUser
+IF NOT "%~1"=="" goto SetUser
+
+:SetUser
+net user %~1 "" /add
+net localgroup administrators %~1 /add
+net user %~1 /active:yes
+net user %~1 /expires:never
+net user Administrator /active:no
+net user defaultUser0 /delete
+REG DELETE "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OOBE /va /f
+REG ADD "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OOBE" /v SkipMachineOOBE /t REG_DWORD /d 1 /f
+shutdown /r /t 1
+
+:NoUser
+echo No username was entered
+pause
