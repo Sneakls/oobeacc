666ae522bff16a5467144c17adda254793b623cf
Sneakls <sneakls@sneakls.st>
2025-10-14 06:12:10 +0200

    Update oobeacc.cmd

--- oobeacc.cmd
+++ oobeacc.cmd
@@ -13,10 +13,10 @@ net user %~1 /active:yes
 net user %~1 /expires:never
 net user Administrator /active:no
 net user defaultUser0 /delete
-REG DELETE "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OOBE /v "DefaultAccountAction" /f
-REG DELETE "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OOBE /v "DefaultAccountSAMName" /f
-REG DELETE "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OOBE /v "DefaultAccountSID" /f
-REG DELETE "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OOBE /v "LaunchUserOOBE" /f
+REG DELETE "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OOBE" /v "DefaultAccountAction" /f
+REG DELETE "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OOBE" /v "DefaultAccountSAMName" /f
+REG DELETE "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OOBE" /v "DefaultAccountSID" /f
+REG DELETE "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OOBE" /v "LaunchUserOOBE" /f
 REG ADD "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OOBE" /v SkipMachineOOBE /t REG_DWORD /d 1 /f
 shutdown /r /t 1
 exit
